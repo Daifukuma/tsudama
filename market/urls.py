018f1ac8ae1bb924023651b2fec055292b227296
mako <g17910fm@gm.tsuda.ac.jp>
2019-10-25 00:25:22 +0900

    ForeignKeyでモデル同士を結びつける

--- market/urls.py
+++ market/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 #    path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='market/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='market/logout.html'), name='logout'),
     path('home/', views.home, name='home'),
     path('users/<int:pk>/', views.users_exhibit, name='users_exhibit'),
     path('photos/new/', views.photos_new, name='photos_new'),
