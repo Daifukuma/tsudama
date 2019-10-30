d17328712d1e0677b28f4dc481c1ba401232d24a
mako <g17910fm@gm.tsuda.ac.jp>
2019-10-30 21:48:40 +0900

    settingのDATABASEの変更

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
 #        'ENGINE': 'django.db.backends.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'Tsudama01',
+        'NAME': 'Tsudama01$default',
         'USER': 'Nakano03',
         'PASSWORD': '3pro1106',
         'HOST': 'mysql.server',
