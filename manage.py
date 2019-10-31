ace5728e18aba041f513d1e10c7600e8aeae0401
mako <g17910fm@gm.tsuda.ac.jp>
2019-10-31 21:03:42 +0900

    設定の変更

--- manage.py
+++ manage.py
@@ -19,7 +19,7 @@ import sys
 
 if __name__ == '__main__':
 #    main()
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tsudama.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 #    os.environ.setdefault('DJANGO_CONFIGURATION','Dev')
     try:
         from django.core.management import execute_from_command_line
