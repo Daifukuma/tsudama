d4d177a759513c481dbdbe2b565412e2bb2401c2
mako <g17910fm@gm.tsuda.ac.jp>
2019-10-31 13:42:00 +0900

    設定の編集

--- manage.py
+++ manage.py
@@ -19,9 +19,11 @@ import sys
 
 if __name__ == '__main__':
 #    main()
-    os.environ.setdefault("DHANGO_SETTINGS_MODULE", "project.settings")
+#    os.environ.setdefault("DHANGO_SETTINGS_MODULE", "project.settings")
+    os.environ.setdefault('DJANGO_CONFIGURATION','Dev')
     try:
         from django.core.management import execute_from_command_line
+        from configurations.management im@ort execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,23 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+#from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
+#application = get_wsgi_application()
+
+path = '/home/Tsudama01/tsudama01.pythonanywhere.com/'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault('AWS_ACCESS_KEY_ID','<SNIP>')
+os.environ.setdefault('AWS_SECRET_ACCESS_KEY','<SNIP>')
+os.environ.setdefaout('DJANGO_SECRET_KEY','<SNIP>')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','projent.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION','Prod')
+
+from configurations.wsgi import get_wsgi_application
 application = get_wsgi_application()
