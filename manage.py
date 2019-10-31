b21bac03d67315f94b1bfcbb682b5c4ee8b3b6f3
mako <g17910fm@gm.tsuda.ac.jp>
2019-10-31 13:48:26 +0900

    設定の変更

--- manage.py
+++ manage.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     os.environ.setdefault('DJANGO_CONFIGURATION','Dev')
     try:
         from django.core.management import execute_from_command_line
-        from configurations.management im@ort execute_from_command_line
+        from configurations.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
