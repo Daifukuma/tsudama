4e7fb0b964229662d3d7e8bcc497e5e7b26ee86b
mako <g17910fm@gm.tsuda.ac.jp>
2019-10-31 20:17:05 +0900

    設定の変更

--- manage.py
+++ manage.py
@@ -19,7 +19,7 @@ import sys
 
 if __name__ == '__main__':
 #    main()
-    os.environ.setdefault("DHANGO_SETTINGS_MODULE", "project.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 #    os.environ.setdefault('DJANGO_CONFIGURATION','Dev')
     try:
         from django.core.management import execute_from_command_line
