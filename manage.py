811d8d3d9ac51e2409807bcc0f7cf169d8792704
mako <g17910fm@gm.tsuda.ac.jp>
2019-11-04 13:19:56 +0900

    変更

--- manage.py
+++ manage.py
@@ -4,10 +4,26 @@ import os
 import sys
 
 
-def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+#def main():
+#    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+#    try:
+#        from django.core.management import execute_from_command_line
+#    except ImportError as exc:
+#        raise ImportError(
+#            "Couldn't import Django. Are you sure it's installed and "
+#            "available on your PYTHONPATH environment variable? Did you "
+#            "forget to activate a virtual environment?"
+#        ) from exc
+#    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+#    main()
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+#    os.environ.setdefault('DJANGO_CONFIGURATION','Dev')
     try:
         from django.core.management import execute_from_command_line
+#        from configurations.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
@@ -15,7 +31,3 @@ def main():
             "forget to activate a virtual environment?"
         )
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
