7c00b31a09ec35e69ac92be52fd0e0f92b1b447b
mako <g17910fm@gm.tsuda.ac.jp>
2019-11-06 01:10:39 +0900

    image表示

--- market/models.py
+++ market/models.py
@@ -193,6 +193,7 @@ class Textbook(models.Model):
     )
     image = models.ImageField(
         verbose_name='画像',
+        widget=FileInputWithPreview,
         upload_to = 'textbooks',
     )
     user = models.ForeignKey(User,on_delete=models.CASCADE)
